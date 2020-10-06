22d0b1dc7fc12e7dbb2529482f5cf31b20aa4b59
Phoebe Kate <pkrosoff@gmail.com>
2020-10-06 11:14:20 -0700

    getting to do lists going

---
+++ static/js/app.js
@@ -0,0 +1,32 @@
+// read in data
+d3.json('samples.json').then(data => {
+    console.log(data);
+})
+// sort ascending
+
+// slice top ten
+
+// make bar chart (.reverse()?)
+
+/// Use sample_values as the values for the bar chart.
+/// Use otu_ids as the labels for the bar chart.
+/// Use otu_labels as the hovertext for the chart.
+
+// create bubble chart
+
+////Use otu_ids for the x values.
+//// Use sample_values for the y values.
+//// Use sample_values for the marker size.
+//// Use otu_ids for the marker colors.
+//// Use otu_labels for the text values.
+
+// Display the sample metadata, i.e., an individual's demographic information.
+
+// Display each key-value pair from the metadata JSON object somewhere on the page.
+
+// Update all of the plots any time that a new sample is selected.
+
+// each individual must have a dashboard with the top ten UTOs and their frequency
+// how do you combine the data?
+// let metadata = data
+// console.log(metadata)
